b9f7e4ff25aa8109c06f0a93df28a06b9440aea9
borisgin <boris.ginsburg@gmail.com>
2017-07-10 17:00:50 -0700

    Removed exoeriments with resnet50_light and with resnet50_selu

--- models/resnet50/train_resnet50_light_fp16.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train --solver=models/resnet50/solver_light_fp16.prototxt -gpu=all \
-    2>&1 | tee models/resnet50/logs/resnet50_light_fp16.log
--- models/resnet50/train_resnet50_selu.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train --solver=models/resnet50/solver_selu.prototxt -gpu=all \
-    2>&1 | tee models/resnet50/logs/resnet50_selu_ramp_dropout.log
