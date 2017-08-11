c36d51f735a8284bbbcd28a34173f4055624075f
Sergei Nikolaev <snikolaev@nvidia.com>
2017-08-10 18:25:19 -0700

    Lint

--- src/caffe/util/im2col.cu
+++ src/caffe/util/im2col.cu
@@ -54,8 +54,7 @@ void im2col_gpu(const Dtype* data_im, const int channels,
   int num_kernels = channels * height_col * width_col;
   cudaStream_t stream = Caffe::thread_stream();
   // NOLINT_NEXT_LINE(whitespace/operators)
-  im2col_gpu_kernel<Dtype>
-      <<<CAFFE_GET_BLOCKS(num_kernels), CAFFE_CUDA_NUM_THREADS, 0, stream>>>(
+  im2col_gpu_kernel<Dtype><<<CAFFE_GET_BLOCKS(num_kernels), CAFFE_CUDA_NUM_THREADS, 0, stream>>>(
       num_kernels, data_im, height, width, kernel_h, kernel_w, pad_h,
       pad_w, stride_h, stride_w, dilation_h, dilation_w, height_col,
       width_col, data_col);
