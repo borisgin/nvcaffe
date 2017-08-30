448ad5b82ea7c4a9353a331b53bcf161e6ace0f3
borisgin <boris.ginsburg@gmail.com>
2017-08-30 15:33:26 -0700

    Added Alexnet_BN for batch=8K with lars

--- models/alexnet_bn/train_alexnet_bn.sh
+++ models/alexnet_bn/train_alexnet_bn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 ./build/tools/caffe train \
-    --solver=models/alexnet_bn/solver_autolr.prototxt -gpu=all \
-    2>&1 | tee models/alexnet_bn/logs/alexnet_bn_autolr.log
+    --solver=models/alexnet_bn/solver.prototxt -gpu=all \
+    2>&1 | tee models/alexnet_bn/logs/alexnet_bn.log
 
