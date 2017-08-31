baac945f47e6e35731fad6294887ad647fbc2c2a
borisgin <boris.ginsburg@gmail.com>
2017-08-31 14:33:09 -0700

    Alexnet_bn with B=16K tuning

--- models/alexnet_bn/train_alexnet_bn_16k.sh
+++ models/alexnet_bn/train_alexnet_bn_16k.sh
@@ -2,5 +2,5 @@
 
 ./build/tools/caffe train \
     --solver=models/alexnet_bn/solver_16k.prototxt -gpu=all \
-    2>&1 | tee models/alexnet_bn/logs/alexnetbn_16k_lr16_fp16.log
+    2>&1 | tee models/alexnet_bn/logs/alexnetbn_16k_lr16_fp24.log
 
