e39cf9c3c8844f18e7f95d892a030976bcefee87
Sergei Nikolaev <snikolaev@nvidia.com>
2017-09-19 20:16:38 -0700

    Handler for user override errors

--- src/caffe/layers/cudnn_conv_layer.cpp
+++ src/caffe/layers/cudnn_conv_layer.cpp
@@ -274,21 +274,41 @@ void CuDNNConvolutionLayer<Ftype, Btype>::AllocateFindExWorkspace() {
 template <typename Ftype, typename Btype>
 size_t CuDNNConvolutionLayer<Ftype, Btype>::AllocateWorkspace(size_t bottom_size) {
   const int dev = Caffe::current_device();
+  cudnnHandle_t handle = Caffe::cudnn_handle();
   for (int i = 0; i < bottom_size; ++i) {
     if (this->phase_ == TRAIN) {
       // get workspace for backwards data algorithm
-      CUDNN_CHECK(cudnnGetConvolutionBackwardDataWorkspaceSize(Caffe::cudnn_handle(),
+      if (CUDNN_STATUS_SUCCESS !=
+          cudnnGetConvolutionBackwardDataWorkspaceSize(handle,
           bwd_filter_desc_, bwd_top_descs_[i], bwd_conv_data_descs_[i], bwd_bottom_descs_[i],
-          bwd_data_algo_[i], &workspace_bwd_data_sizes_[i]));
+          bwd_data_algo_[i], &workspace_bwd_data_sizes_[i])) {
+        // in case of wrong user override
+        bwd_data_algo_[i] = CUDNN_CONVOLUTION_BWD_DATA_ALGO_1;
+        CUDNN_CHECK(cudnnGetConvolutionBackwardDataWorkspaceSize(handle,
+            bwd_filter_desc_, bwd_top_descs_[i], bwd_conv_data_descs_[i], bwd_bottom_descs_[i],
+            bwd_data_algo_[i], &workspace_bwd_data_sizes_[i]));
+      }
       // get workspace for backwards filter algorithm
-      CUDNN_CHECK(cudnnGetConvolutionBackwardFilterWorkspaceSize(Caffe::cudnn_handle(),
+      if (CUDNN_STATUS_SUCCESS !=
+          cudnnGetConvolutionBackwardFilterWorkspaceSize(handle,
           bwd_bottom_descs_[i], bwd_top_descs_[i], bwd_conv_filter_descs_[i], bwd_filter_desc_,
-          bwd_filter_algo_[i], &workspace_bwd_filter_sizes_[i]));
+          bwd_filter_algo_[i], &workspace_bwd_filter_sizes_[i])) {
+        bwd_filter_algo_[i] = CUDNN_CONVOLUTION_BWD_FILTER_ALGO_1;
+        CUDNN_CHECK(cudnnGetConvolutionBackwardFilterWorkspaceSize(handle,
+            bwd_bottom_descs_[i], bwd_top_descs_[i], bwd_conv_filter_descs_[i], bwd_filter_desc_,
+            bwd_filter_algo_[i], &workspace_bwd_filter_sizes_[i]));
+      }
     }
     // get workspace for forwards
-    CUDNN_CHECK(cudnnGetConvolutionForwardWorkspaceSize(Caffe::cudnn_handle(),
+    if (CUDNN_STATUS_SUCCESS !=
+        cudnnGetConvolutionForwardWorkspaceSize(handle,
         fwd_bottom_descs_[i], fwd_filter_desc_, fwd_conv_descs_[i], fwd_top_descs_[i],
-        fwd_algo_[i], &(workspace_fwd_sizes_[i])));
+        fwd_algo_[i], &(workspace_fwd_sizes_[i]))) {
+      fwd_algo_[i] = CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_PRECOMP_GEMM;
+      CUDNN_CHECK(cudnnGetConvolutionForwardWorkspaceSize(handle,
+          fwd_bottom_descs_[i], fwd_filter_desc_, fwd_conv_descs_[i], fwd_top_descs_[i],
+          fwd_algo_[i], &(workspace_fwd_sizes_[i])));
+    }
   }
   CUDA_CHECK(cudaStreamSynchronize(Caffe::thread_stream()));
 
