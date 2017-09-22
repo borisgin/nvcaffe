e98de03dc9a647ce2fa8f348b8947b6f12dd9940
borisgin <boris.ginsburg@gmail.com>
2017-09-21 18:11:58 -0700

    Resnet50 with LARS for DGX station

--- models/resnet50/train_resnet50_lars.sh
+++ models/resnet50/train_resnet50_lars.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 ./build/tools/caffe train --solver=models/resnet50/solver_lars.prototxt -gpu=all \
-    2>&1 | tee models/resnet50/logs/resnet50_lars_b64_lr0.05_e150.log
+    2>&1 | tee models/resnet50/logs/resnet50_lars_b256_lr10.0_e100.log
