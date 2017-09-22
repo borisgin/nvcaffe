da682e4d9b1faeffbecce35fa2e141ae6a2d6f77
borisgin <boris.ginsburg@gmail.com>
2017-09-22 16:30:47 -0700

    Resnet50: lars with B=512

--- models/resnet50/train_resnet50_lars.sh
+++ models/resnet50/train_resnet50_lars.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 ./build/tools/caffe train --solver=models/resnet50/solver_lars.prototxt -gpu=all \
-    2>&1 | tee models/resnet50/logs/resnet50_lars_b256_lr10.0_e100.log
+    2>&1 | tee models/resnet50/logs/resnet50_lars_b512_lr4.0_e100_fp16.log
