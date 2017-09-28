5cee176cfea294092f63dedfff318ef4936f4510
borisgin <boris.ginsburg@gmail.com>
2017-09-28 10:43:15 -0700

    Resnet-50 with LARS and data aug. B=1024

--- models/resnet50/train_resnet50_lars.sh
+++ models/resnet50/train_resnet50_lars.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 ./build/tools/caffe train --solver=models/resnet50/solver_lars.prototxt -gpu=all \
-    2>&1 | tee models/resnet50/logs/resnet50_lars_b512_lr4.0_e100_fp16.log
+    2>&1 | tee models/resnet50/logs/resnet50_lars_b1024_lr9.0_e100_fp16_aug.log
