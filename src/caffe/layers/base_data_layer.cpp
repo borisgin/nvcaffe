a06f358c7d5e2f21fa23505ec4042e9809566e32
Sergei Nikolaev <snikolaev@nvidia.com>
2017-12-15 14:24:14 -0800

    WIP

--- src/caffe/layers/base_data_layer.cpp
+++ src/caffe/layers/base_data_layer.cpp
@@ -238,26 +238,12 @@ template<typename Ftype, typename Btype>
 void BasePrefetchingDataLayer<Ftype, Btype>::Forward_cpu(const vector<Blob*>& bottom,
     const vector<Blob*>& top) {
   // Note: this function runs in one thread per object and one object per one Solver thread
-//  shared_ptr<Batch> batch = prefetches_full_[next_batch_queue_]->pop(
-//      "Data layer prefetch queue empty");
-//  if (top[0]->data_type() == batch->data_->data_type()
-//      && top[0]->shape() == batch->data_->shape()) {
-//    top[0]->Swap(*batch->data_);
-//  } else {
-//    top[0]->safe_reshape_mode(true);
-//    top[0]->CopyDataFrom(*batch->data_, true);
-//  }
-//  if (this->output_labels_) {
-//    if (top[1]->data_type() == batch->label_->data_type()
-//        && top[1]->shape() == batch->label_->shape()) {
-//      top[1]->Swap(*batch->label_);
-//    } else {
-//      top[1]->CopyDataFrom(*batch->label_, true);
-//    }
-//  }
-//  batch->set_id((size_t) -1L);
-//  prefetches_free_[next_batch_queue_]->push(batch);
-//  next_batch_queue();
+  shared_ptr<Batch> batch = this->batch_transformer_->processed_pop();
+  top[0]->Swap(*batch->data_);
+  if (this->output_labels_) {
+    top[1]->Swap(*batch->label_);
+  }
+  this->batch_transformer_->processed_push(batch);
 }
 
 #ifdef CPU_ONLY
