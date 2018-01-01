4202cb1d423946434ada74c21a91019d58249f46
Sergei Nikolaev <snikolaev@nvidia.com>
2017-12-31 16:23:05 -0800

    Ctrl-C

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -391,11 +391,9 @@ void Solver::Finalize() {
   net_->Finalize();
   if (reduce_thread0_) {
     reduce_thread0_->join();
-    reduce_thread0_.reset();
   }
   if (reduce_thread1_) {
     reduce_thread1_->join();
-    reduce_thread1_.reset();
   }
 }
 
