b99782dea73267b7332466d0392a88bbd51bf89a
Sergei Nikolaev <snikolaev@nvidia.com>
2018-01-21 01:32:44 -0800

    lars->larc

--- models/bvlc_alexnet/train_alexnet_16K.sh
+++ models/bvlc_alexnet/train_alexnet_16K.sh
@@ -2,4 +2,4 @@
 
 ./build/tools/caffe train \
     --solver=models/bvlc_alexnet/solver_16K.prototxt -gpu=all \
-    2>&1 | tee models/bvlc_alexnet/logs/alexnet_lars_B16K_lr16_e100.log
+    2>&1 | tee models/bvlc_alexnet/logs/alexnet_larc_B16K_lr16_e100.log
